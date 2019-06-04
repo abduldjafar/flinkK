2a617150dba4a6450ff3957ae3774d73df56ba46
abdul <abdul.haris@dattabot.io>
2019-06-04 11:28:44 +0700

    update BatchJobFilterExamp

--- src/main/java/flinkK/BatchJobFilterExamp.java
+++ src/main/java/flinkK/BatchJobFilterExamp.java
@@ -18,10 +18,15 @@
 
 package flinkK;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.types.IntValue;
+import org.apache.flink.types.StringValue;
+import org.apache.flink.util.Collector;
 
 /**
  * Skeleton for a Flink Batch Job.
@@ -34,16 +39,25 @@ import org.apache.flink.api.common.functions.FilterFunction;
  * and run 'mvn clean package' on the command line.
  */
 public class BatchJobFilterExamp {
-
+	/**
+	 *
+	 * @param args
+	 * @throws Exception
+	 *
+	 * Class ini digunakan untuk mencari video yang paling banyak disukai
+	 * dan jenis apa yang paling banyak disukai
+	 */
 	public static void main(String[] args) throws Exception {
 		// set up the batch execution environment
 		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 
-		DataSet<String> data = env.readTextFile("USvideos.csv")
+		DataSet<Tuple2<String, Integer>> data = env.readTextFile("USvideos.csv")
 				.map(new SplitText())
 				.filter(new FilterLength())
-				.map(new JoinStr());
-		System.out.println(data.count());
+				.flatMap(new Extract())
+				.flatMap(new ConvertToBiasa());
+
+		data.print();
 	}
 
 	private static class SplitText implements MapFunction<String,String[]> {
@@ -57,14 +71,42 @@ public class BatchJobFilterExamp {
 	private static class FilterLength implements FilterFunction<String[]> {
 		@Override
 		public boolean filter(String[] strings) throws Exception {
-			return strings.length == 16;
+			int views = 0;
+			if (!strings[7].equals("views")) {
+				try{
+					views = Integer.parseInt(strings[7]);
+				}catch (NumberFormatException e){
+					views = 0;
+				}
+
+			}
+			return strings.length == 16 && views > 100000;
+
 		}
 	}
 
-	private static class JoinStr implements MapFunction<String[],String> {
+
+	private static class Extract implements FlatMapFunction <String[],Tuple2<StringValue,IntValue>> {
+		// Mutable int field to reuse to reduce GC pressure
+		StringValue judul = new StringValue();
+		IntValue	views = new IntValue();
+
+		// Reuse rating value and result tuple
+		Tuple2<StringValue, IntValue> result = new Tuple2<>(judul, views);
+
 		@Override
-		public String map(String[] s) throws Exception {
-			return String.join(",",s);
+		public void flatMap(String[] data, Collector<Tuple2<StringValue,IntValue>> collector) throws Exception {
+			// Ignore CSV header
+			judul.setValue(data[2]);
+			views.setValue(Integer.parseInt(data[7]));
+			collector.collect(result);
+		}
+	}
+
+	private static class ConvertToBiasa implements FlatMapFunction<Tuple2<StringValue,IntValue>,Tuple2<String, Integer>>{
+		@Override
+		public void flatMap(Tuple2<StringValue, IntValue> data, Collector<Tuple2<String, Integer>> collector) throws Exception {
+			collector.collect(new Tuple2<String, Integer>(data.f0.toString(),Integer.parseInt(data.f1.toString())));
 		}
 	}
 }
